532908a132954b1a2ba0e827de54062f8ea412d3
Ubuntu <ubuntu@groupone>
2016-12-06 18:06:57 +0000

    + seed, generated work

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,33 @@
 Rails.application.routes.draw do
 
+  #get 'work/index'
+
+  #get 'work/choose_theme'
+
+  #get 'work/display_theme'
+
   resources :themes
   resources :images
   resources :values
   resources :users
   root 'main#index'
 
-  get 'main/index'
+  #get 'main/index'
 
-  get 'main/help'
+  #get 'main/help'
 
-  get 'main/contacts'
+  #get 'main/contacts'
 
-  get 'main/about'
+  #get 'main/about'
 
   match 'index', to: 'main#index', via: 'get'
   match 'about', to: 'main#about', via: 'get'
   match 'help', to: 'main#help', via: 'get'
   match 'contacts', to: 'main#contacts', via: 'get'
+
+  match 'work', to: 'work#index', via: 'get'
+  match 'choose_theme', to: 'work#choose_theme', via: 'get'
+  match 'display_theme', to: 'work#display_theme', via: 'get'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Image.delete_all
+Image.reset_pk_sequence
+Image.create([
+    {name: 'bullfight', file: 'Mane bullfight.jpg', theme_id: 2},
+    {name: 'fishing', file: 'Mane fishing.jpg', theme_id: 2},
+    {name: 'peaches', file: 'Mane peaches.jpg', theme_id: 2},
+    {name: 'seascape', file: 'Mane seascape.jpg', theme_id: 2},
+    {name: 'woman with a Jug', file: 'Mane woman with a Jug.jpg', theme_id: 2},
+    {name: 'arlekin', file: 'Picasso arlekin.jpg', theme_id: 3},
+    {name: 'girl before a mirror', file: 'Picasso girl before a mirror.jpg', theme_id: 3},
+    {name: 'gitara', file: 'Picasso gitara.jpg', theme_id: 3},
+    {name: 'muse', file: 'Picasso muse.jpg', theme_id: 3},
+    {name: 'sakre-kyor', file: 'Picasso sakre-kyor.jpg', theme_id: 3}
+             ])
+Theme.delete_all
+Theme.reset_pk_sequence
+Theme.create([
+    {name:'----'}, #1 нет темы
+    {name: 'Какое из изображений художника Мане наилучшим образом характеризует его творчество?'},
+    {name: 'Какое из изображений художника Пикассо наилучшим образом характеризует его творчество?'}
+             ])
