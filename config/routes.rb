5a9a706d060b5e646a4466275bc853cc9876b60a
Ubuntu <ubuntu@groupone>
2016-12-08 18:06:07 +0000

    + api routes

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,13 @@ Rails.application.routes.draw do
   match 'choose_theme', to: 'work#choose_theme', via: 'get'
   match 'display_theme', to: 'work#display_theme', via: 'post'
 
+  namespace :api, defaults: { format: :json } do
+
+    match 'next_image',       to: 'api#next_image',   via: 'get'
+    match 'prev_image',       to: 'api#prev_image',   via: 'get'
+    #match 'save_value',       to: 'api#save_value',   via: :get
+
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
